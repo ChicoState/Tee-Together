c0c17b5057c9f876c2d4b1470b6c17c0914d1be0
PotatoBone <amdavid@mail.csuchico.edu>
2023-09-27 10:58:02 -0700

    init scorecard activity

---
+++ app/src/main/java/com/example/tee_together/ScoreCardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tee_together
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScoreCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scorecard)
+    }
+}
