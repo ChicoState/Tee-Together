54ea4b7f36363ba761436a0b08ccdcae3df95c2d
PotatoBone <amdavid@mail.csuchico.edu>
2023-10-01 14:13:51 -0700

    add new final scorecard review

--- app/src/main/java/com/example/tee_together/ScoreCardActivity.kt
+++ app/src/main/java/com/example/tee_together/ScoreCardActivity.kt
@@ -1,8 +1,11 @@
 package com.example.tee_together
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -14,57 +17,28 @@ class ScoreCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scorecard)
-        var scorecardHandler = ScoreCard()
+        var scorecardHandler = ScoreCardHandler()
         val addHole = findViewById<FloatingActionButton>(R.id.add_hole)
         val containerScores = findViewById<LinearLayout>(R.id.scores_for_holes)
+        val changeToResult = findViewById<ImageButton>(R.id.change_to_result_scorecard)
         addHole.setOnClickListener {
-            /*
-            // Create a new widget for a players score
-            val newScore = LinearLayout(this)
-            newScore.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            )
-            newScore.orientation = LinearLayout.HORIZONTAL
-            newScore.setBackgroundResource(R.drawable.cell_shape)
-            containerScores.addView(newScore)
-            val holeNumber = TextView(this)
-            holeNumber.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            holeNumber.setPadding(16)
-            val holeToPrint = holeCount + 1
-            holeNumber.text = "Hole $holeToPrint"
-            holeCount += 1
-            val incrementButton = ImageButton(this)
-            incrementButton.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            incrementButton.setImageResource(R.drawable.baseline_add_24)
-            incrementButton.setPadding(10)
-            val decrementButton = ImageButton(this)
-            decrementButton.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            decrementButton.setImageResource(R.drawable.baseline_remove_24)
-            decrementButton.setPadding(10)
-            val holeScore = TextView(this)
-            holeScore.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            holeScore.gravity = Gravity.END
-            decrementButton.setPadding(10)
-             */
             scorecardHandler.createNewHole(containerScores, this)
         }
+        changeToResult.setOnClickListener(View.OnClickListener {
+            if (scorecardHandler.getStrokesForHoles().toIntArray().isNotEmpty()) {
+                val intent = Intent(this, ScoreCardResultActivity::class.java)
+                intent.putExtra("player_names", "Player 1")
+                intent.putExtra(
+                    "strokes_per_holes",
+                    scorecardHandler.getStrokesForHoles().toIntArray()
+                )
+                startActivity(intent)
+            }
+        })
     }
 }
 
-class ScoreCard{
+class ScoreCardHandler{
     private var holeCount = 0
     private var scores = mutableListOf<Int>()
 
@@ -84,6 +58,7 @@ class ScoreCard{
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
         holeNumber.setPadding(16)
+        Log.d("Hello", "hello $holeCount")
         val holeToPrint = holeCount + 1
         holeNumber.text = "Hole $holeToPrint"
         holeCount += 1
@@ -110,8 +85,8 @@ class ScoreCard{
         holeScore.setPadding(16)
         val currScore = scores[holeCount - 1]
         holeScore.text = "Score: $currScore"
+        val index = holeCount - 1
         decrementButton.setOnClickListener {
-            val index = holeCount - 1
             if (scores[index] > 0){
                 scores[index] -= 1
             }
@@ -119,7 +94,6 @@ class ScoreCard{
             holeScore.text = "Score: $score"
         }
         incrementButton.setOnClickListener {
-            val index = holeCount - 1
             scores[index] += 1
             val score = scores[index]
             holeScore.text = "Score: $score"
@@ -129,5 +103,8 @@ class ScoreCard{
         newScore.addView(incrementButton)
         newScore.addView(decrementButton)
         newScore.addView(holeScore)
+    }
+    fun getStrokesForHoles(): MutableList<Int> {
+        return scores;
     }
 }
---
+++ app/src/main/java/com/example/tee_together/ScoreCardResultActivity.kt
@@ -0,0 +1,85 @@
+package com.example.tee_together
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setPadding
+import com.google.android.material.bottomappbar.BottomAppBar
+
+class ScoreCardResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scorecard_result)
+        val tableLayoutContainer = findViewById<TableLayout>(R.id.scorecard_table)
+        var scoreCardResultHandler = ScoreCardResultHandler()
+        scoreCardResultHandler.createResult("Player 1", intent.getIntArrayExtra("strokes_per_holes"), tableLayoutContainer, this)
+        val navigateBack = findViewById<BottomAppBar>(R.id.bottomAppBarScorecardResult)
+        navigateBack.setOnClickListener{
+            finish()
+        }
+    }
+}
+
+class ScoreCardResultHandler(){
+    fun createResult(playerNames: String, strokes: IntArray?, container: TableLayout, context: Context){
+
+        // Begin by iterating through player names and adding each player
+        val players = TableRow(context)
+        players.gravity = Gravity.CENTER
+        val header = TextView(context)
+        header.layoutParams = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+        header.text = "Players: "
+        header.setPadding(8)
+        header.setBackgroundResource(R.drawable.cell_shape)
+        // For now, just one player :: TODO::REPLACE WITH ITERATION
+        val player = TextView(context)
+        player.layoutParams = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+        player.text="$playerNames"
+        player.setPadding(8)
+        player.setBackgroundResource(R.drawable.cell_shape)
+
+        players.addView(header)
+        players.addView(player)
+        container.addView(players)
+
+        //Create recorded strokes per hole
+        var count = 1
+        if (strokes != null) {
+            for (stroke in strokes){
+                val hole = TableRow(context)
+                hole.gravity = Gravity.CENTER
+                val holeNumber = TextView(context)
+                holeNumber.layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT
+                )
+                holeNumber.setBackgroundResource(R.drawable.cell_shape)
+                holeNumber.text = "Hole $count"
+                count += 1
+                holeNumber.setPadding(8)
+                //TODO::Replace with for loop to iterate through each players score
+                val strokePerPlayer = TextView(context)
+                strokePerPlayer.layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT
+                )
+                strokePerPlayer.text="$stroke"
+                strokePerPlayer.setPadding(8)
+                strokePerPlayer.setBackgroundResource(R.drawable.cell_shape)
+                hole.addView(holeNumber)
+                hole.addView(strokePerPlayer)
+                container.addView(hole)
+            }
+        }
+    }
+}
