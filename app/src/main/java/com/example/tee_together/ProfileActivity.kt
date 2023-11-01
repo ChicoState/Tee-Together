e3df19513af5da14ba532b0147c08304f085a00c
tyrodgers18 <tfrodgers@csuchico.edu>
2023-11-01 13:46:57 -0700

    Fixed #24 - Navigation buttons have functionality now

--- app/src/main/java/com/example/tee_together/ProfileActivity.kt
+++ app/src/main/java/com/example/tee_together/ProfileActivity.kt
@@ -4,11 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val scorecardButton: ImageButton = findViewById<ImageButton>(R.id.scorecardButton)
+        scorecardButton.setOnClickListener {
+            val intent = Intent(this, ScoreCardActivity::class.java)
+            startActivity(intent)
+        }
+        val profileButton: ImageButton = findViewById<ImageButton>(R.id.profileButton)
+        profileButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
