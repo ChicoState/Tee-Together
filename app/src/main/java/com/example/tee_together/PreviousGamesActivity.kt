a2c81035f13acd8cd06d2e4790572d6a53e343e4
AaronDavid <amdavid@mail.csuchico.edu>
2023-11-05 11:56:28 -0800

    blah

--- app/src/main/java/com/example/tee_together/PreviousGamesActivity.kt
+++ app/src/main/java/com/example/tee_together/PreviousGamesActivity.kt
@@ -3,6 +3,8 @@ package com.example.tee_together
 import android.os.Bundle
 import android.widget.TableLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class PreviousGamesActivity : AppCompatActivity() {
@@ -12,5 +14,15 @@ class PreviousGamesActivity : AppCompatActivity() {
         val tableLayoutContainer = findViewById<TableLayout>(R.id.scorecard_table)
         var scoreCardResultHandler = ScoreCardResultHandler()
         scoreCardResultHandler.createResult(intent.getStringExtra("player_names"), intent.getIntArrayExtra("strokes_per_holes"), tableLayoutContainer, this)
+    }
+}
+
+class PreviousGameBuilder(){
+    fun build(){
+        val db = FirebaseFirestore.getInstance()
+        var auth = FirebaseAuth.getInstance()
+        var uid = auth.uid.toString()
+
+        db.collection("users").document(uid).collection("games").get()
     }
 }
